b680464ed3f5353c408085318e77e99e58f1399c
joker012-yazid <yazidmohd058@gmail.com>
2025-09-10 13:19:32 +0800

    feat: add prisma schema and docker setup

--- api/src/main.ts
+++ api/src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.enableCors({ origin: 'http://localhost:8080' });
+  await app.listen(8081);
 }
 bootstrap();
